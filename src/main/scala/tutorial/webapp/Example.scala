d55b37e77f2c871aee0519b9cd90b0a8ab72c9ba
janeturner <jane.turner@digital.hmrc.gov.uk>
2019-09-04 15:23:35 +0100

    changes added

---
+++ src/main/scala/tutorial/webapp/Example.scala
@@ -0,0 +1,53 @@
+package tutorial.webapp
+
+import scala.scalajs.js.Math
+
+object Color {
+  def rgb(r: Int, g: Int, b: Int) = s"rgb($r, $g, $b)"
+  val White = rgb(255, 255, 255)
+  val Red = rgb(255, 0, 0)
+  val Green = rgb(0, 255, 0)
+  val Blue = rgb(0, 0, 255)
+  val Turquoise = rgb(0, 255, 255)
+  val Magenta = rgb(255, 0, 255)
+  val Yellow = rgb(255, 255, 0)
+  val Black = rgb (0, 0, 0)
+  val Orange = rgb(255, 165, 0)
+  val Grey = rgb(192, 192, 192)
+  val all = Seq(
+    White,
+    Red,
+    Green,
+    Blue,
+    Turquoise,
+    Magenta,
+    Yellow,
+    Black,
+    Orange,
+    Grey
+  )
+}
+
+case class Point(x: Double, y: Double){
+  def +(other: Point) = Point(x + other.x, y + other.y)
+  def -(other: Point) = Point(x - other.x, y - other.y)
+  def %(other: Point) = Point(x % other.x, y % other.y)
+  def <(other: Point) = x < other.x && y < other.y
+  def >(other: Point) = x > other.x && y > other.y
+  def /(value: Double) = Point(x / value, y / value)
+  def *(value: Double) = Point(x * value, y * value)
+  def *(other: Point) = x * other.x + y * other.y
+  def length = Math.sqrt(lengthSquared)
+  def lengthSquared = x * x + y * y
+  def within(a: Point, b: Point, extra: Point = Point(0, 0)) = {
+    import math.{min, max}
+    x >= min(a.x, b.x) - extra.x &&
+      x < max(a.x, b.x) + extra.y &&
+      y >= min(a.y, b.y) - extra.x &&
+      y < max(a.y, b.y) + extra.y
+  }
+  def rotate(theta: Double) = {
+    val (cos, sin) = (Math.cos(theta), math.sin(theta))
+    Point(cos * x - sin * y, sin * x + cos * y)
+  }
+}
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,40 +1,104 @@
 package tutorial.webapp
 
 
+
 import org.scalajs.dom
 import dom.document
+import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.raw.HTMLImageElement
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
+import tutorial.webapp.Color._
 
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    addParagraph("Hello Word")
-  }
-
-  def addParagraph(in: String): Unit = {
-    document.body.appendChild(document.createElement("p").appendChild(document.createTextNode(in)))
-    addButton()
-  }
-
-  @JSExportTopLevel("addClickedMessage")
-  def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+    createGame()
   }
 
 
-  def addButton() = {
-    val button = document.createElement("button")
-    button.innerHTML= "Click Me!"
-    button.setAttribute("onClick", "addClickedMessage()")
-    document.body.appendChild(button)
-  }
+  def createGame() = {
 
+    var playerX = 100
+    var playerY = 190
+
+
+    var obstacleX = 590
+    var obstacleSpeed = 3
+
+    val canvas = dom.document.createElement("canvas").asInstanceOf[Canvas]
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+    canvas.width = 600
+    canvas.height = 400
+
+
+    dom.document.body.appendChild(canvas)
+
+    def drawThings(): Unit = {
+      //draw Canvas
+      val bgImage = new Image("images/background.png")
+      ctx.drawImage(bgImage.element, 0, 0, canvas.width, canvas.height)
+     // drawRect(0, 0, canvas.width, canvas.height, Black)
+
+      val flyingPig = new Image("images/flying-pig1.png")
+      ctx.drawImage(flyingPig.element, playerX, playerY, 100, 100)
+      //draw player
+      //drawRect(playerX, playerY, 15, 15, Blue)
+
+      //draw obstacle
+      drawRect(obstacleX, 200, 10, 200, Magenta)
+
+      if (playerX == obstacleX) stopGame()
+
+    }
+
+    def drawRect(x: Double, y: Double, width: Int, height: Int, colour: String) = {
+      ctx.fillStyle = colour
+      ctx.fillRect(x, y, width, height)
+    }
+
+    def moveThings(): Unit = {
+      obstacleX -= obstacleSpeed
+    }
+
+    def stopGame() = {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+    class Image(src: String) {
+      private var ready: Boolean = false
+
+      val element = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
+      element.onload = (e: dom.Event) => ready = true
+      element.src = src
+
+      def isReady:Boolean = ready
+    }
+
+    class Component(x: Int, y: Int, width: Int, height: Int, colour: String) {
+      def draw() {
+        drawRect(x, y, width, height, colour)
+      }
+    }
+
+
+
+    js.timers.setInterval(28) {
+      drawThings()
+      moveThings()
+    }
+
+    dom.window.addEventListener("keydown", (p: dom.KeyboardEvent) => {
+      p.keyCode match {
+        case 37 => playerX -= 3
+        case 38 => playerY -= 3
+        case 39 => playerX += 3
+        case 40 => playerY += 3
+        case _ => println("the keycode is " + p.keyCode)
+      }
+    }, false)
+
+
+  }
 }
